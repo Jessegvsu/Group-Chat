c5689a454a3fbb9aafc1bd2e71886329e65b2c85
Jesse Sage <jessegreen909@gmail.com>
2026-04-08 20:45:14 -0400

    Client and server files created. Started implementing threads and multiple client functionality

--- client.py
+++ client.py
@@ -1,1 +1,37 @@
+import socket
+import sys
+if __name__ == '__main__':
+    #Connects to server
+    print("Type port number:")
+    while True:
+        try:
+            port = int(input())
+            print("Connected to", port)
+            break
+        except (ValueError, TypeError):
+            print("Please enter a valid port number:")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect(("localhost", port))
+
+        #create loop for interacting with server
+        while True:
+            data = input()
+            if data == "system: quit":
+                sock.sendall(data.encode("utf-8"))
+                sock.sendall(b"\n")
+                sock.close()
+                break
+            sock.sendall(data.encode("utf-8"))
+            sock.sendall(b"\n")
+            received = str(sock.recv(1024), "utf-8")
+            print("Received:", received)
+    print("program closed")
+
+
+
+
+
+
+
 
--- server.py
+++ server.py
@@ -1,22 +1,53 @@
-from threading import Thread
-from socketserver import StreamRequestHandler, TCPServer
-from socket import socket, AF_INET, SOCK_STREAM, create_connection, gaierror
-class GroupChat(StreamRequestHandler):
+import socketserver
+import threading
+from socketserver import TCPServer
 
+class GroupChat(socketserver.BaseRequestHandler):
+    def listen(self, name):
+        #
     def handle(self):
-        #recieve message nad put into list.
-        print(f"Received from {self.client_address}")
-        client_request_message = []
-        while True:
-            line = self.rfile.readline(1000)
-            print(f"From client: {line}")
-            client_request_message.append(line)
-            if (len(line) == 2) or (len(line) == 0):
-                break
 
-        #send message back to client.
-        for line in client_request_message:
-            self.wfile.write(line)
+        names = []
+        threads = []
+        #parses message
+
+        try:
+            while True:
+                pieces =[b'']
+                total = 0
+                while b'\n' not in pieces[-1] and total < 10_000:
+                    pieces.append(self.request.recv(2000))
+                    total += len(pieces[-1])
+                self.data = b''.join(pieces)
+                name = self.data[:10].decode("utf-8")
+
+                #if the name included in client message has not been recieved,
+                # add it to names and open new thread with name and start thread.
+                if name not in names:
+                    names.append(name)
+                    thread = threading.Thread(target=self.listen, args=name)
+                    threads.append(thread)
+                    thread.start()
+
+                #print data from client for server use
+                print(f"Received from: {self.client_address[0]}:")
+                print(f"Recieved message: {self.data.decode("utf-8")}")
+
+                #if client message is system quit close socket to client
+                #   MODIFY TO ACCOUNT FOR THREADS
+                if self.data == b"system: quit":
+                    self.request.close()
+                    break
+
+                #send message back to client reflecting what it is.
+                #   REMOVE TO ACCOUNT FOR SENDING TO MULTIPLE CLIENTS
+                self.request.sendall(f"Server recieved: {self.data}".encode("utf-8"))
+
+        except ConnectionResetError:
+            self.request.close()
+            print("Connection reset by peer")
+
+
 
 if __name__ == '__main__':
 
@@ -25,7 +56,8 @@ if __name__ == '__main__':
         HOST, PORT = "localhost", 5000
         TCPServer.allow_reuse_port = True
         TCPServer.allow_reuse_address = True
-        with TCPServer((HOST, PORT), GroupChat) as server:
+        with socketserver.TCPServer((HOST, PORT), GroupChat) as server:
             server.serve_forever()
+
     except KeyboardInterrupt:
         print("Keyboard interrupt detected")
